f584a14cb09d0bd207700ca575cea981850b5c2d
Urvashi Singh <us229253@gmail.com>
2025-11-11 19:52:35 +0530

    fix refresh token

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -171,7 +171,7 @@ await User.findByIdAndUpdate(
 const refreshAccessToken = asyncHandler(async (req, res) => {
   const incomingRefreshToken =  req.cookies.refreshToken || req.body.refreshToken
 
-  if(incomingRefreshToken){
+  if(!incomingRefreshToken){
     throw new ApiError(401, "unauthorized request")
   }
 
